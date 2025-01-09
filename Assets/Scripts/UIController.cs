0fb8a48260d3af7c19c6ddea5a874c7a39d46acd
SamuelRodri <72737491+SamuelRodri@users.noreply.github.com>
2025-01-09 22:54:18 +0000

    Game UI Done

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
-    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text scoreText1;
+    [SerializeField] private TMP_Text scoreText2;
     [SerializeField] private GameObject gameOverPanel;
-    [SerializeField] private TMP_Text highScore;
+    [SerializeField] private TMP_Text highScore1;
+    [SerializeField] private TMP_Text highScore2;
     [SerializeField] private GameObject key;
 
     public void UpdateScore(int score)
-        => scoreText.text = $"Score: {score}";
+    {
+        scoreText1.text = $"Score: {score}";
+        scoreText2.text = $"Score: {score}";
+    }
 
     public void ShowGameOver(int score)
     {
-        highScore.text = $"High Score: {score}";
+        highScore1.text = $"High Score: {score}";
+        highScore2.text = $"High Score: {score}";
         gameOverPanel.SetActive(true);
     }
 
